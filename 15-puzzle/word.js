22015185a2f558e0bb58ba33e93ccc88d760fd5e
KangSungHyun94 <shkang6753@gmail.com>
2020-10-22 18:34:45 +0900

    15-puzzle
    단어 퍼즐 3번 맞추면 다맞췄다고 alert으로 알려줌.
    한번 다맞추고 초기화할때 btns만 shift해주면 전에있던 단어의 버튼들 다 없어질줄 알았는데, word2의 태그들을 removeChild 해줬어야 했음. 이부분이 해결하기 어려웠고 구글링 통해서 해결고, btns shift해도 안없어지길래 그곳에 console.log 넣어봤는데 잘 실행이 됐었고, btns shift는 원할히 이루어 젔지만 화면에서 버튼들이 안 없어젔었음.

--- 15-puzzle/word.js
+++ 15-puzzle/word.js
@@ -1,44 +1,129 @@
-var str = document.getElementById('word1').innerHTML;
-var word2 = document.getElementById('word2');
+//html elements
+var word2 = document.getElementById('word2'); //buttons
+var word1 = document.getElementById('word1'); //answer
+var check = document.getElementById('check'); //word1 === word2?
+var progress = document.getElementById('progress'); //porgress check
 
-var game={};
-game.word=str.split('');
-game.btns=[];
+//game objects
+var game={
+    'btns':[],
+    'maxPlay':3,
+    'current':0
+};
+game.words = 'apple,linux,javascript,tutorial,codesquad,baby,girlfriend,legend'.split(',');
 
-for (var i = 0; i < str.length; i++) {
-    var btn = document.createElement('button');
-    btn.innerHTML = str[i];
-    word2.appendChild(btn);
-    game.btns.push(btn);
-}
-
-var swap = function () {
-    var a =game.word.length-1;
-    var strg;
-    for( var i = 0; i < a-i; i++){
-        strg=game.word[i];
-        game.word[i]=game.word[a-i];
-        game.word[a-i]=strg;
+//chose 1 word from words;
+game.choose = function() {
+    var idx = Math.floor(Math.random()*this.words.length)
+    this.answer=this.words[idx]; //answer 선택된 문자열 들어감.
+    this.letters = this.answer.split(''); //letters에는 선택된 문자열을 한글자씩 쪼개서 배열로 만듦.
+    word1.innerHTML=this.answer;
+};
+game.addButtons= function(){
+    for (var i = 0; i < this.letters.length; i++) {
+        var btn = document.createElement('button');
+        btn.innerHTML = this.letters[i];
+        word2.appendChild(btn);
+        this.btns.push(btn);
     }
-    game.copyBtnText();
 };
 
-game.copyBtnText=function(){
-    for(var i=0; i < this.word.length; i++){
-        this.btns[i].innerHTML=this.word[i];
+game.checkGood=function(){
+    return this.answer===this.letters.join('');
+};
+
+game.updateDisplay = function(){
+    if(game.checkGood()){
+        check.innerHTML='일치합니다.';
+    }else {
+        check.innerHTML='일치하지 않습니다.';
     }
-}
+};
+
+game.init=function(){
+    this.choose();
+    this.addButtons();
+    this.updateDisplay();
+    
+};
+game.init();
+
+game.copyBtnText=function(){
+    for(var i=0; i < this.letters.length; i++){
+        this.btns[i].innerHTML=this.letters[i];
+    }
+};
+
+game.swap=function(){
+    var temp=[];
+    //coply and swap
+    while (game.letters.length !=0){
+        var s = game.letters.pop();
+        temp.push(s);
+    }
+    game.letters=temp;
+    game.copyBtnText();
+    game.updateDisplay();
+};
+
+game.rshift=function(){
+    var s = game.letters.pop();
+    game.letters.unshift(s);
+    game.copyBtnText();
+    game.updateDisplay();
+};
+game.lshift=function(){
+    var s = game.letters.shift();
+    game.letters.push(s);
+    game.copyBtnText();
+    game.updateDisplay();
+};
+
+game.progress=function(){
+    if(game.checkGood()){
+        game.current++;
+        for(var i=0; i<this.answer.length; i++){
+            this.btns.shift();
+            word2.removeChild(word2.childNodes[0]);
+        }
+        game.init();
+        var str = "";
+        for (var i = 0; i < game.current;i++){
+            str +="0";
+        }
+        progress.innerHTML=str;
+    }
+    if (game.current==game.maxPlay){
+        alert("Good! Thank you for playing");
+    }
+};
+//evnt handler for swap button
+var swap = function () {
+    game.swap();
+    game.progress();
+};
 
 var rshift = function () {
-    
-    var s = game.word.pop();
-    game.word.unshift(s);
-    game.copyBtnText();
+    game.rshift();
+    game.progress();
 };
 
 var lshift = function () {
-    
-    var s = game.word.shift();
-    game.word.push(s);
-    game.copyBtnText();
+    game.lshift();
+    game.progress();
 };
+//shuffle
+game.shuffle = function () {
+    var toggle = Math.floor(Math.random() * 2) === 0;
+
+    if (toggle) {
+        game.swap();
+    }
+
+    var n = Math.floor(Math.random() * game.letters.length);
+
+    for (var i = 0; i < n; i++) {
+        game.rshift();
+    }
+};
+game.shuffle();
